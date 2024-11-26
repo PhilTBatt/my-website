c00e72cbcea24dde96049b1eca74058f47d4ccf1
PhilTBatt <philtbatt@gmail.com>
2024-11-26 16:06:52 +0000

    Better header

--- phils-website/src/Components/Header.tsx
+++ phils-website/src/Components/Header.tsx
@@ -1,14 +1,15 @@
 export default function Header() {
     return (
         <section>
-            <h1>
-                Philip Battersby
-            </h1>
+            <div>
+                <h1>
+                    Philip Battersby
+                </h1>
+                <a href="mailto:philtbatt@gmail.com">philtbatt@gmail.com</a>
+                <p>07786184893</p>
+            </div>
             <a href="https://github.com/PhilTBatt">GitHub</a>
             <a href="inkedin.com/in/philip-battersby">LinkedIn</a>
-            <br/>
-            <a href="mailto:philtbatt@gmail.com">philtbatt@gmail.com</a>
-            <p>07786184893</p>
         </section>
     )
 }
