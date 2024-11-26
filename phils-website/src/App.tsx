892a61334989db48d4f8c93b53e4c8b1a30d9645
PhilTBatt <philtbatt@gmail.com>
2024-11-26 15:35:04 +0000

    Projects

--- phils-website/src/App.tsx
+++ phils-website/src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './Components/Header'
+import Projects from './Components/Projects'
 
 function App() {
   return (
     <>
     <Header/>
+    <Projects/>
     </>
   )
 }
--- phils-website/src/Components/Projects.tsx
+++ phils-website/src/Components/Projects.tsx
@@ -1,11 +1,17 @@
 export default function Projects() {
     return (
         <section>
-            Projects
+            <h2>My Projects</h2>
             <br/>
-            <ul>
-                
-            </ul>
+            <div>
+                <a>Clowder - Cat Tracking Web App</a>
+            </div>
+            <div>
+                <a href="https://philtbatt-ncnews.netlify.app/" target="blank">Northcoders News Site</a>
+            </div>
+            <div>
+                <a href="https://be-nc-news-oovq.onrender.com/api" target="_blank">Northcoders News API</a>
+            </div>
         </section>
     )
 }
