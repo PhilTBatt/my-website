76d3ce5fbdb4ef3082c40141bfe23aab035c5060
PhilTBatt <philtbatt@gmail.com>
2024-11-26 15:09:06 +0000

    Base components set up

---
+++ phils-website/src/Components/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+    return (
+        <section>
+            <h1>
+                Philip Battersby
+            </h1>
+            <a href="https://github.com/PhilTBatt"></a>
+            <a href="inkedin.com/in/philip-battersby"></a>
+            <br/>
+            <a href="mailto:philtbatt@gmail.com"></a>
+            07786184893
+        </section>
+    )
+}
