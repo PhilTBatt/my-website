3b73c7cef7a05b348a3948445c1affd44f11c8bb
PhilTBatt <philtbatt@gmail.com>
2024-11-27 00:27:36 +0000

    Help

--- phils-website/tailwind.config.js
+++ phils-website/tailwind.config.js
@@ -1,6 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts,tsx, css}"],
+  content: ["./src/**/*.{html,js,ts,tsx}",
+    "./index.html"
+  ],
   theme: {
     extend: {},
   },
