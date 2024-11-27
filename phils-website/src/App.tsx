f3b7b3e73cd9d605c9e949efc1b1cc19ea1395ab
PhilTBatt <philtbatt@gmail.com>
2024-11-27 16:45:23 +0000

    Footer

--- phils-website/src/App.tsx
+++ phils-website/src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import CV from './Components/CV'
+import Footer from './Components/Footer'
 import Header from './Components/Header'
 import Projects from './Components/Projects'
 
@@ -9,6 +10,7 @@ function App() {
     <Header/>
     <Projects/>
     <CV/>
+    <Footer/>
     </>
   )
 }
---
+++ phils-website/src/Components/Footer.tsx
@@ -0,0 +1,9 @@
+export default function Footer() {
+    return (
+        <section className="section-box">
+            <p className="text-[3.3vw]">
+                This website was made with Reaxt, Vite, Typescript and Tailwind
+            </p>
+        </section>
+    )
+}
